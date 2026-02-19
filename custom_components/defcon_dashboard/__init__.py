ac4fb468e4b8b325f9a229527d058dd8ac19c70a
CyberCon_Security_Solutions <cyberconsecurity@users.noreply.github.com>
2026-02-19 15:48:12 -0500

    Add future annotations import for type hints

--- custom_components/defcon_dashboard/__init__.py
+++ custom_components/defcon_dashboard/__init__.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from .const import DOMAIN
+
 PLATFORMS = ["sensor"]
 
 async def async_setup(hass: HomeAssistant, config: dict):
